13509a0416e3466c68d1c711693411e8691cdfb9
Abhijeet Singh Baghel <abhijeetbaghel77@gmail.com>
2026-01-19 15:58:13 +0530

    linked list basics

--- lecture6/class6.java
+++ lecture6/class6.java
@@ -9,6 +9,7 @@ class node{// node creation using  class
         this.next=null;
     }
 }
+
 class linkedlist{
     node head;
     void insertAtBeginning(int data){
@@ -24,15 +25,38 @@ class linkedlist{
         }
         System.out.println("null");
     }
+    void add_last(int data){
+        node newnode=new node(data);
+        if(head==null){
+            head=newnode;
+            
+        }
+        node temp=head;
+        while(temp.next!=null){
+            temp=temp.next;
+        }
+        temp.next=newnode;
+
+
+        
+    }
+    void delete_first(){
+        head=head.next;
+
+    }
+    
 }
 public class class6 {
     public static void main(String[] args) {
         linkedlist list=new linkedlist();
         list.insertAtBeginning(10);
-        list.insertAtBeginning(20);
-        list.insertAtBeginning(30);
-        list.insertAtBeginning(40);
-        list.insertAtBeginning(50);
+        list.add_last(20);
+        list.add_last(30);
+        list.add_last(40);
+        list.add_last(50);
+       
+        list.display();
+        list.delete_first();
         list.display();
     }
 }
